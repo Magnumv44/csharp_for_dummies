45ba08bfeb48c69cdc27edb6d0de8a2439e99620
Magnumv44 <vitaliy1984@gmail.com>
2021-10-31 18:43:46 +0200

    Пример из третьей главы

---
+++ ch3/ModifyString/ModifyString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+/*
+ * ModifyString - 
+ * 
+ */
+namespace ModifyString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            s1.Name = "Дженни";
+
+            Student s2 = new Student();
+            s2.Name = s1.Name;
+
+            s2.Name = s1.Name.ToUpper();
+            Console.WriteLine("s1 - " + s1.Name + ", s2 - " + s2.Name);
+
+            // Ожидаем подтверждения от пользователя для окончания работы
+            Console.WriteLine("Нажмите клавишу <Enter> для выхода...");
+            Console.Read();
+        }
+    }
+
+    class Student
+    {
+        public String Name;
+    }
+}
