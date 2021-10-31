c703703c8d29ee372cb741401ee4023d70bd5a68
Magnumv44 <vitaliy1984@gmail.com>
2021-10-31 18:43:26 +0200

    Пример из первой главы

---
+++ ch1/Program1/Program1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Запрос на ввод имени пользователя
+            Console.WriteLine("Введите ваше имя:");
+
+            // Считываем введенное имя
+            string name = Console.ReadLine();
+
+            // Выводим на экран привествие пользователю с введенным именем
+            Console.WriteLine("Привет, " + name);
+
+            // Ожидаем подтверждения от пользователя для окончания работы
+            Console.WriteLine("Нажмите клавишу <Enter> для выхода...");
+            Console.Read();
+        }
+    }
+}
